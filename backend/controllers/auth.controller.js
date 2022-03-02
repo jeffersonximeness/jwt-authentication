58c0c6c6a93f2bbeac79cbe13fc5e8d79c9601cb
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-03-02 15:17:44 -0300

    função de signin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const db = require('../models')
-const config = require('../config')
+const config = require('../config/auth.config')
 let jwt = require('jsonwebtoken')
 let bcrypt = require('bcryptjs')
 
@@ -29,6 +29,44 @@ exports.signup = (req, res) => {
         else {
             user.setRoles([1]).then(() => res.send({ message: 'User was registered successfully!' }))
         }
+    }).catch(err => {
+        res.status(500).send({ message: err.message })
+    })
+}
+
+exports.signin = (req, res) => {
+
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if (!user) {
+            return res.status(404).send({ message: 'User not found.' })
+        }
+
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+
+        if (!passwordIsValid) {
+            return res.status(401).send({ accessToken: null, message: 'Invalid password.' })
+        }
+
+        let token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 }) // 24 hours
+
+        let authorities = []
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+                authorities.push(`ROLES_${roles[i].name.toUpperCase()}`)
+            }
+
+            res.status(200).send({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                roles: authorities,
+                accessToken: token
+            })
+        })
     }).catch(err => {
         res.status(500).send({ message: err.message })
     })
