f7012aad0b73f8a3f74055266945acf60d568fc1
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-03-02 14:49:15 -0300

    função para verificar se é admin ou moderador

--- backend/middleware/authJwt.js
+++ backend/middleware/authJwt.js
@@ -55,3 +55,32 @@ const isModerator = (req, res, next) => {
         })
     })
 }
+
+const isModeratorOrAdmin = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+                if (roles[i].name === 'admin') {
+                    next()
+                    return
+                }
+                
+                if (roles[i].name === 'moderator') {
+                    next()
+                    return
+                }
+            }
+
+            res.status(403).send({ message: 'Require Moderator or Admin role' })
+        })
+    })
+}
+
+const authJwt = {
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
+    isModerator: isModerator,
+    isModeratorOrAdmin: isModeratorOrAdmin
+}
+
+module.exports = authJwt
