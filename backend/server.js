12aaaf16885cb87bb4b61f0450dd2886b64ceb21
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-03-04 12:27:10 -0300

    correções no Dockerfile

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to application' })
+    res.json({ message: 'Welcome to my application' })
 })
 
 const Role = db.role
